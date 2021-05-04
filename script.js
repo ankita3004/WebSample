43585d2f8a8864a87c49a8052a77a70fa179810c
ankita3004 <56727715+ankita3004@users.noreply.github.com>
2021-05-04 23:06:04 +0530

    Update Files

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function myFunction() { //function defination//
+        var element = document.body;
+        element.classList.toggle("dark");//linking with css
+        var ele=document.querySelector('.begin');
+        ele.classList.toggle("light");
+        }
